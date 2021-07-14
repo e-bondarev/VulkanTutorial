44ad127f285073f72a3a87015323e58aa7f4c0ee
e-bondarev <82723220+e-bondarev@users.noreply.github.com>
2021-07-14 08:58:02 +0200

    Update pch.h

--- src/pch.h
+++ src/pch.h
@@ -10,17 +10,42 @@
 #include <array>
 #include <set>
 
-#define LOG_OUT(...) 	spdlog::info(__VA_ARGS__)
-#define ERR_OUT(...) 	spdlog::error(__VA_ARGS__)
-#define WARN_OUT(...) 	spdlog::warn(__VA_ARGS__)
-#define TRACE()			LOG_OUT("[Call]: " __FUNCTION__)
+#define VOID_ASSEMBLY			(void(0))
 
-#define THROW(...)					\
-	ERR_OUT(__VA_ARGS__);			\
-	throw std::runtime_error("")
+#ifdef NDEBUG
+#	define RELEASE
+#else
+#	define DEBUG
+#endif
 
-#define VK_CHECK(x, msg)			\
-	if (x != VK_SUCCESS)			\
-	{								\
-		THROW(msg);					\
-	}
+#ifdef RELEASE
+#	define DISABLE_LOG
+#	define DISABLE_CHECKS
+#endif
+
+#ifndef DISABLE_LOG
+#	define LOG_OUT(...) 		spdlog::info(__VA_ARGS__)
+#	define ERR_OUT(...) 		spdlog::error(__VA_ARGS__)
+#	define WARN_OUT(...) 		spdlog::warn(__VA_ARGS__)
+#	define TRACE()				LOG_OUT("[Call]: " __FUNCTION__)
+#else
+#	define LOG_OUT(...) 		VOID_ASSEMBLY
+#	define ERR_OUT(...) 		VOID_ASSEMBLY
+#	define WARN_OUT(...) 		VOID_ASSEMBLY
+#	define TRACE()				VOID_ASSEMBLY
+#endif
+
+#ifndef DISABLE_CHECKS
+#	define THROW(...)					\
+		ERR_OUT(__VA_ARGS__);			\
+		throw std::runtime_error("")
+
+#	define VK_CHECK(x, msg)				\
+		if (x != VK_SUCCESS)			\
+		{								\
+			THROW(msg);					\
+		}
+#else
+#	define THROW(...)			VOID_ASSEMBLY
+#	define VK_CHECK(x, msg)		x
+#endif
